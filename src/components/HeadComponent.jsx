d68a6225e325d8fe1340d58f14f5d9833c1f4df9
ikokywu <rikiridwan949@gmail.com>
2024-12-03 21:33:02 +0700

    feat: add helmet configuration and website logo

---
+++ src/components/HeadComponent.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import PropTypes from "prop-types";
+import WebLogo from "../../public/web-logo.png";
+
+const HeadComponent = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <link rel="icon" href={WebLogo} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="My Blog" />
+      <meta property="og:url" content={window.location.href} />
+    </Helmet>
+  );
+};
+
+HeadComponent.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  keywords: PropTypes.string,
+};
+
+export default HeadComponent;

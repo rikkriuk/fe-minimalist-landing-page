73b5cb65f8536ed6ba6d8f1c5019bf2828b2ee2a
ikokywu <rikiridwan949@gmail.com>
2024-12-03 20:48:56 +0700

    test: add testing to FooterComponent.jsx

---
+++ src/__test__/components/FooterComponent.test.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FooterComponent from "../../components/FooterComponent";
+
+describe("FooterComponent", () => {
+   test("renders company logo", () => {
+      render(
+        <BrowserRouter>
+          <FooterComponent />
+        </BrowserRouter>
+      );
+  
+      const logoImage = screen.getByAltText("Company-logo");
+      expect(logoImage).toBeInTheDocument();
+   });
+
+   test("renders 'What We Do' section", () => {
+      const { container } = render(
+         <BrowserRouter>
+           <FooterComponent />
+         </BrowserRouter>
+       );
+     
+      const footer = container.querySelector("footer");
+      const sectionHeading = footer.querySelector("h4");
+      expect(sectionHeading).toHaveTextContent("What We Do");
+   }); 
+  
+   test("renders email address", () => {
+      render(
+        <BrowserRouter>
+          <FooterComponent />
+        </BrowserRouter>
+      );
+  
+      const emailText = screen.getByText("info@dsgnr.com");
+      expect(emailText).toBeInTheDocument();
+   });
+  
+   test("renders links correctly", () => {
+      render(
+        <BrowserRouter>
+          <FooterComponent />
+        </BrowserRouter>
+      );
+
+      const linkItem = ["Intro", "Home", "Work", "Pricing", "Contact Us", "About", "News", "Testimonial"];
+   
+      linkItem.forEach((link) => {
+         expect(screen.getByText(link)).toBeInTheDocument();
+      })
+   });
+})
